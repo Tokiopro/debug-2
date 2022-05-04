f04053f416fe3a1416dd1c28ef7cd74d788b23a8
Tokiopro <tokisaonana@yahoo.co.jp>
2022-05-04 09:05:22 +0000

    削除機能追加

--- config/routes.rb
+++ config/routes.rb
@@ -5,8 +5,8 @@ Rails.application.routes.draw do
   devise_for :users
   resources :books, only: [:index,:show,:edit,:create,:destroy,:update] do
     resource :favorites, only: [:create, :destroy]
-    resource :book_comments, only: [:create]
-    
+  resources :book_comments, only: [:create, :destroy]
+
   end
   resources :users, only: [:index,:show,:edit,:update]
 
